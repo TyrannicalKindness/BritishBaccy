e879927a9bda204074ea1352d26668aab7d54fea
TyrannicalKindness <Mountaintree96@gmail.com>
2025-07-13 18:57:19 +0100

    Update firebase-config.js to use environment variables

--- firebase-config.js
+++ firebase-config.js
@@ -32,7 +32,38 @@ const firebaseClientConfig = {
 // 8. Uncomment the code below
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+// firebase-config.js
+const firebase = require('firebase/app');
+require('firebase/auth');
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Export for use in server.js
+module.exports = { auth, firebase };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
